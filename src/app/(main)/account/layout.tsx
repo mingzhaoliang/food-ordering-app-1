ceb96128a01732f3571af5b39e85ea969b5df9f5
mingzhaoliang <timothymc.leung@gmail.com>
2024-04-06 19:45:37 +1100

    style: Change account page background and layout
    Other changes: Refactor display-image

--- src/app/(main)/account/layout.tsx
+++ src/app/(main)/account/layout.tsx
@@ -4,15 +4,14 @@ import { images } from "@/utils/data";
 
 export default function AccountPageLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="w-screen min-h-screen flex justify-center items-start">
+        <>
             <DisplayImage
-                src={images.hero0.src}
-                alt={images.hero0.alt}
-                imageRef={images.hero0.ref}
-                imageClasses="fixed top-0 left-0 w-full min-h-screen -z-10 object-cover object-center shadow-md transition-all"
-                textClasses="bottom-0 left-0"
+                src={images.background0.src}
+                alt={images.background0.alt}
+                imageRef={images.background0.ref}
+                className="w-screen h-screen fixed top-0 left-0 -z-10 opacity-85 overflow-hidden"
             />
-            <div className="relative z-40 mt-24 sm:mt-28 lg:mt-36 mb-16 p-2 sm:p-4 rounded-md w-11/12 sm:w-10/12 md:w-9/12 max-w-[65rem] bg-white/40 backdrop-blur space-y-2 md:space-y-4 gap-4 lg:gap-6 transition-all">
+            <div className="relative z-40 mt-32 sm:mt-36 lg:mt-40 mb-16 mx-auto p-2 sm:p-4 rounded-md w-11/12 sm:w-10/12 md:w-9/12 max-w-[65rem] bg-white/40 backdrop-blur space-y-2 md:space-y-4 transition-all">
                 <div className="bg-white rounded-md">
                     <Sidebar />
                 </div>
@@ -20,6 +19,6 @@ export default function AccountPageLayout({ children }: { children: React.ReactN
                     {children}
                 </div>
             </div>
-        </div>
+        </>
     )
 }
--- src/components/ui/display-image.tsx
+++ src/components/ui/display-image.tsx
@@ -3,7 +3,7 @@
 import Image, { StaticImageData } from "next/image";
 import { useState } from "react";
 
-export default function DisplayImage({ src, alt, imageRef, width, height, imageClasses, textClasses, priority }: { src: StaticImageData, alt: string, imageRef: JSX.Element, width?: number, height?: number, imageClasses: string, textClasses: string, priority?: boolean }) {
+export default function DisplayImage({ src, alt, imageRef, width, height, className, priority }: { src: StaticImageData | string, alt: string, imageRef: React.ReactNode, width?: number, height?: number, className: string, priority?: boolean }) {
     const [isHovered, setIsHovered] = useState(false);
 
     const mouseOverHandler = () => {
@@ -15,26 +15,28 @@ export default function DisplayImage({ src, alt, imageRef, width, height, imageC
     }
 
     return (
-        <>
+        <div className={`${className}`}>
             <Image
                 src={src}
                 alt={alt}
                 width={width}
                 height={height}
+                fill={!width || !height}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 draggable={false}
-                className={imageClasses}
+                className="w-full h-full object-cover object-center"
                 onMouseOver={mouseOverHandler}
                 onMouseOut={mouseOutHandler}
                 priority={priority || false}
             />
             <p
-                className={`absolute px-4 bg-white/50 text-sm ${textClasses}`}
+                className="absolute px-4 bg-white/50 text-sm bottom-0 left-0"
                 style={{ display: isHovered ? "block" : "none", }}
                 onMouseOver={mouseOverHandler}
                 onMouseOut={mouseOutHandler}
             >
                 {imageRef}
             </p>
-        </>
+        </div>
     )
 }
