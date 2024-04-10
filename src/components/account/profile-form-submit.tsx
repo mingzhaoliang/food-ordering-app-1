cbd68d1b36e39c4ab44b193f2ffcd8b18172d389
mingzhaoliang <timothymc.leung@gmail.com>
2024-04-10 23:49:49 +1000

    style: Update profile save button style

--- src/components/account/profile-form-submit.tsx
+++ src/components/account/profile-form-submit.tsx
@@ -6,7 +6,7 @@ export default function ProfileFormSubmit() {
     const { pending } = useFormStatus();
 
     return (
-        <button type="submit" className="py-2 px-6 rounded-md border text-slate-800 border-slate-800 hover:bg-slate-800 hover:text-white transition-all duration-300">
+        <button type="submit" disabled={pending} className={`py-2 px-6 rounded-md border ${pending ? "border-slate-600 bg-slate-600 text-white cursor-not-allowed" : "text-slate-800 border-slate-800 hover:bg-slate-800 hover:text-white"} transition-all duration-300`}>
             {pending ? "Saving..." : "Save"}
         </button>
     )
