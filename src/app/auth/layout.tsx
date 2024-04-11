481b09b6c90157a07454c91734b4aafafd91b002
mingzhaoliang <timothymc.leung@gmail.com>
2024-04-11 21:14:47 +1000

    other: Change image file location

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -10,12 +10,11 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
                 alt={images.authBackground.alt}
                 imageRef={images.authBackground.ref}
                 priority
-                imageClasses="fixed top-0 left-0 w-screen h-screen z-0 object-cover overflow-hidden"
-                textClasses="bottom-0 left-0 z-0"
+                className="fixed top-0 left-0 w-screen h-screen z-0 object-cover overflow-hidden"
             />
             <div className="z-10 shadow-xl max-[480px]:px-6 max-[480px]:pt-8 px-8 pt-10 pb-4 sm:min-w-80 xl:min-w-96 bg-white rounded-xl flex flex-col justify-center items-center gap-4 transition-all">
                 <div className="rounded-full border-2 border-amber-300 p-[0.1rem] w-16 h-16 aspect-square flex justify-center items-center shadow animate-spin-slow">
-                    <Image src={images.pizza.src} alt="Pizza" className="rounded-full border border-amber-300 p-2 object-cover" />
+                    <Image src="/icons/pizza.png" alt="Pizza" width={60} height={60} className="rounded-full border border-amber-300 object-cover p-2" />
                 </div>
                 {children}
             </div>
--- src/components/navigation/logo.tsx
+++ src/components/navigation/logo.tsx
@@ -1,5 +1,4 @@
 import { useAppSelector } from "@/lib/store/hooks";
-import { images } from "@/utils/data";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -13,7 +12,7 @@ export default function Logo() {
             className={`lg:w-3/12 lg:min-w-[22rem] px-4 xs:px-6 py-2 flex gap-2 xs:gap-4 justify-center items-center ${activePage === "home" && !showHeaderBackground && "lg:shadow-md"}`}
         >
             <div className={`max-[380px]:hidden w-10 h-10 xs:w-12 xs:h-12 aspect-square rounded-full border-2 p-[0.1rem] flex justify-center items-center shadow animate-spin-medium ${activePage === "home" && "lg:border-slate-400"} ${showHeaderBackground || showMenu ? "border-slate-400" : "border-white bg-white/50"}`}>
-                <Image src={images.pizza.src} alt="Pizza" className={`p-[0.4rem] object-cover rounded-full border  ${activePage === "home" && "lg:border-slate-400"} ${showHeaderBackground || showMenu ? "border-slate-400" : "border-white"}`} />
+                <Image src="/icons/pizza.png" alt="Pizza" width={45} height={45} className={`p-[0.4rem] object-cover rounded-full border  ${activePage === "home" && "lg:border-slate-400"} ${showHeaderBackground || showMenu ? "border-slate-400" : "border-white"}`} />
             </div>
             <h1 className={`text-lg min-[380px]:text-[1.35rem] xs:text-2xl sm:text-3xl xl:text-4xl font-black tracking-wider font-cursive ${showHeaderBackground || showMenu ? "text-slate-800" : `text-white ${activePage === "home" && "lg:text-slate-800"}`}`}>Cucina Felice</h1>
         </Link>
