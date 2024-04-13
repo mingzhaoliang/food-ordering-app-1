c611bacb9c446831309e2bd161d22aa50d4af6f4
mingzhaoliang <timothymc.leung@gmail.com>
2024-04-14 00:25:05 +1000

    style: Update button font size

--- src/components/my/orders/further-actions/buttons/cancelled-order-actions.tsx
+++ src/components/my/orders/further-actions/buttons/cancelled-order-actions.tsx
@@ -1,6 +1,6 @@
 export default function CancelledOrderActions() {
     return (
-        <button className="text-sm xs:text-base lg:text-lg xl:text-xl w-full text-center p-2 rounded font-lato text-white bg-slate-700 hover:bg-slate-900 transition-all duration-300">
+        <button className="text-sm xxs:text-base md:text-lg w-full text-center p-2 rounded font-lato text-white bg-slate-700 hover:bg-slate-900 transition-all duration-300">
             Re-order
         </button>
     )
--- src/components/my/orders/further-actions/buttons/paid-order-actions.tsx
+++ src/components/my/orders/further-actions/buttons/paid-order-actions.tsx
@@ -1,6 +1,6 @@
 export default function PaidOrderActions() {
     return (
-        <div className="flex gap-2 text-sm xs:text-base lg:text-lg xl:text-xl">
+        <div className="flex gap-2 text-sm xxs:text-base md:text-lg">
             <button className="w-full text-center p-2 rounded font-lato text-slate-800 border border-slate-800 hover:text-white hover:bg-slate-800 transition-all duration-300">
                 Cancel
                 <span className="hidden xs:inline"> Order</span>
