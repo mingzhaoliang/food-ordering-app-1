8ad5b5247da5cc8a65e3154f195b148b392c0e1b
mingzhaoliang <timothymc.leung@gmail.com>
2024-05-02 13:01:06 +1000

    fix: Fix build error
    Fix client fetch error on /api/auth/providers

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,14 +1,28 @@
-import { getProviders } from "next-auth/react";
+"use client";
+
+import { ClientSafeProvider, LiteralUnion, getProviders } from "next-auth/react";
 import { restaurantName } from "@/utils/data";
 import SignInForm from "@/components/auth/signin-form";
 import AuthContent from "@/components/auth/auth-content";
+import { useEffect, useState } from "react";
+import { BuiltInProviderType } from "next-auth/providers/index";
 
-export default async function SignInPage() {
-    const providers = await getProviders();
+export default function SignInPage() {
+	const [providers, setProviders] = useState<Record<
+		LiteralUnion<BuiltInProviderType, string>,
+		ClientSafeProvider
+	> | null>(null);
 
-    return (
-        <AuthContent title="Welcome" message={`Sign in to continue to ${restaurantName}.`}>
-            <SignInForm providers={providers} />
-        </AuthContent>
-    );
+	useEffect(() => {
+		(async () => {
+			const res = await getProviders();
+			setProviders(res);
+		})();
+	}, []);
+
+	return (
+		<AuthContent title="Welcome" message={`Sign in to continue to ${restaurantName}.`}>
+			<SignInForm providers={providers} />
+		</AuthContent>
+	);
 }
